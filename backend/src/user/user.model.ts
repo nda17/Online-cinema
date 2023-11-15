1193fd748cfa8a09ca8fceb40140ecd12dc027cf
nfdmitry <khv1702@gmail.com>
2023-11-15 23:49:43 +0300

    Сonfigured and tested working with the mongodb database

---
+++ backend/src/user/user.model.ts
@@ -0,0 +1,18 @@
+import { prop } from '@typegoose/typegoose'
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses'
+// eslint-disable-next-line
+export interface UserModel extends Base {}
+// eslint-disable-next-line
+export class UserModel extends TimeStamps {
+	@prop({ unique: true })
+	email: string
+
+	@prop()
+	password: string
+
+	@prop({ default: false })
+	isAdmin?: boolean
+
+	@prop({ default: [] })
+	favorites?: []
+}
