480202f739b88dc8940c70a555de8b8898bc2ec0
nfdmitry <khv1702@gmail.com>
2023-11-24 00:34:41 +0300

    Added new object "Movie", files: movie.controller.ts, movie.service.ts, create-movie.dto.ts, movie.model.ts, movie.module.ts.

---
+++ backend/src/movie/dto/create-movie.dto.ts
@@ -0,0 +1,43 @@
+import { IsArray, IsNumber, IsObject, IsString } from 'class-validator'
+
+export class Parameters {
+	@IsNumber()
+	year: number
+
+	@IsNumber()
+	duration: number
+
+	@IsString()
+	country: number
+}
+
+export class CreateMovieDto {
+	@IsString()
+	poster: string
+
+	@IsString()
+	bigPoster: string
+
+	@IsString()
+	title: string
+
+	@IsString()
+	description: string
+
+	@IsString()
+	slug: string
+
+	@IsObject()
+	parameters?: Parameters
+
+	@IsString()
+	videoUrl: string
+
+	@IsArray()
+	@IsString({ each: true })
+	genres: string[]
+
+	@IsArray()
+	@IsString({ each: true })
+	actors: string[]
+}
