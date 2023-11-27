e9b4966c21e8c19fe21c1170777b2f2e6aa2741f
nfdmitry <khv1702@gmail.com>
2023-11-28 00:46:33 +0300

    Implementation of functionality for adding and removing a movie from the "Favorites" list

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { TypegooseModule } from 'nestjs-typegoose'
+import { ActorModule } from './actor/actor.module'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
 import { getMongoDbConfig } from './config/mongo.config'
 import { FileModule } from './file/file.module'
 import { GenreModule } from './genre/genre.module'
+import { MovieModule } from './movie/movie.module'
 import { UserModule } from './user/user.module'
-import { ActorModule } from './actor/actor.module';
-import { MovieModule } from './movie/movie.module';
 
 @Module({
 	imports: [
